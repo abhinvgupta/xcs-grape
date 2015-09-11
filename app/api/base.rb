915c0a091118b994b1dfe158072549b44f282451
abhinav <ruffneck@Admins-MacBook-Pro-4.local>
2015-09-11 11:36:21 +0530

    4th

--- app/api/base.rb
+++ app/api/base.rb
@@ -9,5 +9,36 @@ module API
 		get 'front' do 
 			{"lname" => params["lastname"],"fname" => params["firstname"]}
 		end
+
+		resource 'permissions' do 
+			get 'tableadd' do
+				params do
+					requires :module,type: :string
+					requires :act,type: :string
+				end
+				mod = params[:module]
+				act = params[:act]
+				name = mod + "." + act
+				PermissionTable.create(:name => name)
+			end
+			post 'matrixadd' do
+				params do
+					requires :module,type: :string
+					requires :act,type: :string
+					requires :role,type: :string
+				end
+				pername = params[:module] + "." + params[:act]
+				
+				per_id = PermissionTable.where(:name => pername).pluck(:id)
+				roleid = Role.where(:name => params[:role]).pluck(:id)
+				if !per_id.nil? && !roleid.nil?
+					PermissionMatrix.create(:role_id => roleid[0],:permission_id => per_id[0])
+				
+				else
+					{:error => "enter params"}
+				end
+				
+			end
+		end
 	end
 end
